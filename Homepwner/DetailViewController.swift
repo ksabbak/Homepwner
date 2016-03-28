1a0a3aa7bfdd741f752e8b27ef35ac6d5eeca68e
k_sabbak <k_sabbak@Hogwarts-2.local>
2016-03-28 16:50:24 -0500

    More chapter 12

---
+++ Homepwner/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by k_sabbak on 3/28/16.
+//  Copyright © 2016 k_sabbak. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController
+{
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    
+    @IBOutlet var dateLabel: UILabel!
+}
